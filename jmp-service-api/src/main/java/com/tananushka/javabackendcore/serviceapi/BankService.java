cca71ecf9dda02f4644048466468074c7d09ca84
Kir Tananushka <kir.tananushka@verifone.com>
2024-11-10 15:11:34 +0400

    Add average user age and payable user check methods

--- jmp-service-api/src/main/java/com/tananushka/javabackendcore/serviceapi/BankService.java
+++ jmp-service-api/src/main/java/com/tananushka/javabackendcore/serviceapi/BankService.java
@@ -4,6 +4,8 @@ import com.tananushka.javabackendcore.dto.BankCard;
 import com.tananushka.javabackendcore.dto.Subscription;
 import com.tananushka.javabackendcore.dto.User;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +15,19 @@ public interface BankService {
    Optional<Subscription> getSubscriptionByBankCardNumber(String bankCardNumber);
 
    List<User> getAllUsers();
+
+   static boolean isPayableUser(User user) {
+      return calculateAge(user) >= 18;
+   }
+
+   private static long calculateAge(User user) {
+      return ChronoUnit.YEARS.between(user.birthday(), LocalDate.now());
+   }
+
+   default double getAverageUsersAge() {
+      return getAllUsers().stream()
+            .mapToLong(BankService::calculateAge)
+            .average()
+            .orElse(0.0);
+   }
 }
