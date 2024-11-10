334339fcbc2a44e7fa35c064c054c19920910160
Kir Tananushka <kir.tananushka@verifone.com>
2024-11-10 11:46:00 +0400

    Add bank implementations and card number generator

---
+++ jmp-cloud-bank-impl/src/main/java/com/tananushka/javabackendcore/bankimpl/RetailBank.java
@@ -0,0 +1,27 @@
+package com.tananushka.javabackendcore.bankimpl;
+
+import com.tananushka.javabackendcore.bankapi.Bank;
+import com.tananushka.javabackendcore.dto.BankCard;
+import com.tananushka.javabackendcore.dto.BankCardType;
+import com.tananushka.javabackendcore.dto.CreditBankCard;
+import com.tananushka.javabackendcore.dto.DebitBankCard;
+import com.tananushka.javabackendcore.dto.User;
+import com.tananushka.javabackendcore.utils.SimpleCardNumberGenerator;
+
+public class RetailBank implements Bank {
+   @Override
+   public BankCard createBankCard(User user, BankCardType bankCardType) {
+      String cardNumber = SimpleCardNumberGenerator.generateCardNumber();
+
+      return switch (bankCardType) {
+         case CREDIT -> CreditBankCard.builder()
+               .number("RETAIL-CREDIT-" + cardNumber)
+               .user(user)
+               .build();
+         case DEBIT -> DebitBankCard.builder()
+               .number("RETAIL-DEBIT-" + cardNumber)
+               .user(user)
+               .build();
+      };
+   }
+}
