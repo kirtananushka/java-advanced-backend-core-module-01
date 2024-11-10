cef9d43933e5bd7d05aa74c848ff8124cb7c5baa
Kir Tananushka <kir.tananushka@verifone.com>
2024-11-10 18:30:36 +0400

    Add getAllSubscriptionsByCondition

--- jmp-cloud-service-impl/src/main/java/com/tananushka/javabackendcore/serviceimpl/ServiceImpl.java
+++ jmp-cloud-service-impl/src/main/java/com/tananushka/javabackendcore/serviceimpl/ServiceImpl.java
@@ -4,13 +4,14 @@ import com.tananushka.javabackendcore.dto.BankCard;
 import com.tananushka.javabackendcore.dto.Subscription;
 import com.tananushka.javabackendcore.dto.User;
 import com.tananushka.javabackendcore.entity.SubscriptionEntity;
+import com.tananushka.javabackendcore.exception.JmpException;
 import com.tananushka.javabackendcore.serviceapi.BankService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,15 +34,23 @@ public class ServiceImpl implements BankService {
    }
 
    @Override
-   public Optional<Subscription> getSubscriptionByBankCardNumber(String bankCardNumber) {
+   public Subscription getSubscriptionByBankCardNumber(String bankCardNumber) {
       return subscriptionRepository.findById(bankCardNumber)
-            .map(Subscription::toDto);
+            .map(Subscription::toDto).orElseThrow(() -> new JmpException("Subscription not found"));
    }
 
    @Override
    public List<User> getAllUsers() {
       return userRepository.findAll().stream()
             .map(User::toDto)
+            .collect(Collectors.toUnmodifiableList());
+   }
+
+   @Override
+   public List<Subscription> getAllSubscriptionsByCondition(Predicate<Subscription> subscriptionPredicate) {
+      return subscriptionRepository.findAll().stream()
+            .map(Subscription::toDto)
+            .filter(subscriptionPredicate)
             .collect(Collectors.toUnmodifiableList());
    }
 }
--- jmp-service-api/src/main/java/com/tananushka/javabackendcore/serviceapi/BankService.java
+++ jmp-service-api/src/main/java/com/tananushka/javabackendcore/serviceapi/BankService.java
@@ -7,15 +7,9 @@ import com.tananushka.javabackendcore.dto.User;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.Optional;
+import java.util.function.Predicate;
 
 public interface BankService {
-   void subscribe(BankCard bankCard);
-
-   Optional<Subscription> getSubscriptionByBankCardNumber(String bankCardNumber);
-
-   List<User> getAllUsers();
-
    static boolean isPayableUser(User user) {
       return calculateAge(user) >= 18;
    }
@@ -23,6 +17,14 @@ public interface BankService {
    private static long calculateAge(User user) {
       return ChronoUnit.YEARS.between(user.birthday(), LocalDate.now());
    }
+
+   void subscribe(BankCard bankCard);
+
+   Subscription getSubscriptionByBankCardNumber(String bankCardNumber);
+
+   List<User> getAllUsers();
+
+   List<Subscription> getAllSubscriptionsByCondition(Predicate<Subscription> subscriptionPredicate);
 
    default double getAverageUsersAge() {
       return getAllUsers().stream()
