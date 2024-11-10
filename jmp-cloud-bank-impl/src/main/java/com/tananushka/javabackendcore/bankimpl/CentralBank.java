11a8dc2b0960a79d209c33a5bfe05f530da0ef86
Kir Tananushka <kir.tananushka@verifone.com>
2024-11-10 18:34:49 +0400

    Refactor bank card creation logic into BankCardFactory

--- jmp-cloud-bank-impl/src/main/java/com/tananushka/javabackendcore/bankimpl/CentralBank.java
+++ jmp-cloud-bank-impl/src/main/java/com/tananushka/javabackendcore/bankimpl/CentralBank.java
@@ -3,27 +3,14 @@ package com.tananushka.javabackendcore.bankimpl;
 import com.tananushka.javabackendcore.bankapi.Bank;
 import com.tananushka.javabackendcore.dto.BankCard;
 import com.tananushka.javabackendcore.dto.BankCardType;
-import com.tananushka.javabackendcore.dto.CreditBankCard;
-import com.tananushka.javabackendcore.dto.DebitBankCard;
 import com.tananushka.javabackendcore.dto.User;
-import com.tananushka.javabackendcore.utils.SimpleCardNumberGenerator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CentralBank implements Bank {
+
    @Override
    public BankCard createBankCard(User user, BankCardType bankCardType) {
-      String cardNumber = SimpleCardNumberGenerator.generateCardNumber();
-
-      return switch (bankCardType) {
-         case CREDIT -> CreditBankCard.builder()
-               .number("CENTRAL-CREDIT-" + cardNumber)
-               .user(user)
-               .build();
-         case DEBIT -> DebitBankCard.builder()
-               .number("CENTRAL-DEBIT-" + cardNumber)
-               .user(user)
-               .build();
-      };
+      return BankCardFactory.createBankCard("CENTRAL", user, bankCardType);
    }
 }
