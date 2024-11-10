d6f7566924817648cc035d0b7bb2468acaa22442
Kir Tananushka <kir.tananushka@verifone.com>
2024-11-10 15:12:20 +0400

    Refactor entity-to-DTO conversions

--- jmp-cloud-service-impl/src/main/java/com/tananushka/javabackendcore/serviceimpl/ServiceImpl.java
+++ jmp-cloud-service-impl/src/main/java/com/tananushka/javabackendcore/serviceimpl/ServiceImpl.java
@@ -4,7 +4,6 @@ import com.tananushka.javabackendcore.dto.BankCard;
 import com.tananushka.javabackendcore.dto.Subscription;
 import com.tananushka.javabackendcore.dto.User;
 import com.tananushka.javabackendcore.entity.SubscriptionEntity;
-import com.tananushka.javabackendcore.entity.UserEntity;
 import com.tananushka.javabackendcore.serviceapi.BankService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,20 +28,20 @@ public class ServiceImpl implements BankService {
       subscriptionRepository.save(subscriptionEntity);
 
       var user = bankCard.getUser();
-      var userEntity = new UserEntity(user.name(), user.surname(), user.birthday());
+      var userEntity = user.toEntity();
       userRepository.save(userEntity);
    }
 
    @Override
    public Optional<Subscription> getSubscriptionByBankCardNumber(String bankCardNumber) {
       return subscriptionRepository.findById(bankCardNumber)
-            .map(entity -> new Subscription(entity.getBankcardNumber(), entity.getStartDate()));
+            .map(Subscription::toDto);
    }
 
    @Override
    public List<User> getAllUsers() {
       return userRepository.findAll().stream()
-            .map(entity -> new User(entity.getName(), entity.getSurname(), entity.getBirthday()))
-            .toList();
+            .map(User::toDto)
+            .collect(Collectors.toUnmodifiableList());
    }
 }
